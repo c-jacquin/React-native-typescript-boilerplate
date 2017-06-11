7dffd080038f1dd1431b7c4ee250c7ef8cb3db1f
Charles Jacquin <charles.jacquin@autistici.org>
2017-06-11 22:10:45 +0200

    chore(generator): add components generator

---
+++ __generators__/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const componentGenerator = require('./component/index.js')
+const containerGenerator = require('./container/index.js')
+
+const capitalizeFirstLetter = (string) => `${string.charAt(0).toUpperCase()}${string.slice(1)}`
+
+module.exports = (plop) => {
+    plop.setGenerator('component', componentGenerator)
+    plop.setGenerator('container', containerGenerator)
+    plop.addHelper('directory', (comp) => {
+        try {
+            fs.accessSync(`src/containers/${comp}`, fs.F_OK)
+            return `containers/${comp}`
+        } catch (e) {
+            return `components/${comp}`
+        }
+    })
+    plop.addHelper('docParam', (comp) => `{${capitalizeFirstLetter(comp)}}`)
+    plop.addHelper('curly', (object, open) => (open ? '{' : '}'))
+    plop.addHelper('capitalize', (comp) => comp.toUpperCase())
+}
