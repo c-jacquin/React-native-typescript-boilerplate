4aaed4df641788598048b086446d91d3d1d75a6a
Charles Jacquin <charles.jacquin@autistici.org>
2017-06-12 01:00:41 +0200

    feat(ENV): add NODE_ENV variable and load confi accordingly

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,15 @@
+import devConfig from './env/development'
+import prodConfig from './env/production'
+
+const extraConfig = process.env.NODE_ENV === 'development' ? devConfig : prodConfig
+
 export default {
-  appName: 'MyApp',
-  storeKey: 'myAppStoreKey',
-  language: {
-    defaultLocale: 'en',
-    supportedLocales: ['fr', 'en'],
-  },
-  actionsToPersist: [],
+    appName: 'MyApp',
+    storeKey: 'myAppStoreKey',
+    language: {
+        defaultLocale: 'en',
+        supportedLocales: ['fr', 'en'],
+    },
+    actionsToPersist: [],
+    ...extraConfig,
 }
