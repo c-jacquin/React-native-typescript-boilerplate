a33124aee36007364f4388b3be1305b30305fff5
Charles Jacquin <charles.jacquin@autistici.org>
2017-07-23 14:51:16 +0200

    refactor(redux): dev env

--- src/store/logger/epic.ts
+++ src/store/logger/epic.ts
@@ -2,10 +2,7 @@ import { Epic } from 'redux-observable'
 import { AppState, ReduxAction } from '../types'
 
 const logEpic: Epic<ReduxAction, AppState> = action$ => {
-    return action$
-        .filter(() => process.env.NODE_ENV === 'development')
-        .do(console.log)
-        .filter(() => false)
+    return action$.do(console.log).filter(() => false)
 }
 
 export default logEpic
