88bf84784a291589a471c62ee748eff62844e5fb
Charles Jacquin <charles.jacquin@autistici.org>
2017-07-23 14:51:16 +0200

    refactor(redux): dev env

--- src/store/rootEpic.ts
+++ src/store/rootEpic.ts
@@ -4,9 +4,12 @@ import languageEpic from './language/epic'
 import logEpic from './logger/epic'
 import { AppState, ReduxAction } from './types'
 
-const rootEpic: Epic<ReduxAction, AppState> = combineEpics(
-    languageEpic,
-    logEpic
-)
+const epics = [languageEpic]
+
+if (process.env.NODE_ENV === 'development') {
+    epics.push(logEpic)
+}
+
+const rootEpic: Epic<ReduxAction, AppState> = combineEpics(...epics)
 
 export default rootEpic
