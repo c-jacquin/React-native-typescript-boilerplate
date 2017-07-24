6c9e9a89bc224537fbad791299a59c4f977b48ad
Charles Jacquin <charles.jacquin@autistici.org>
2017-07-24 19:30:57 +0200

    test(selectors): add tests for language selectors and refactor generators (#10)

--- src/store/rootEpic.ts
+++ src/store/rootEpic.ts
@@ -1,10 +1,15 @@
+import 'rxjs/Rx'
 import { combineEpics, Epic } from 'redux-observable'
 
+import { AppState, ReduxAction } from './types'
 import languageEpic from './language/epic'
 import logEpic from './logger/epic'
-import { AppState, ReduxAction } from './types'
+// Import epic here
 
-const epics = [languageEpic]
+const epics = [
+    languageEpic,
+    // Insert epic here
+]
 
 if (process.env.NODE_ENV === 'development') {
     epics.push(logEpic)
