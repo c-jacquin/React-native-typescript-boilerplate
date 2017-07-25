bff396e6b40376db521872b931cb03583fd2a551
Charles Jacquin <charles.jacquin@autistici.org>
2017-07-25 17:30:02 +0200

    refactor(logger): remove the logger epic

--- src/store/logger/epic.ts
+++
@@ -1,8 +0,0 @@
-import { Epic } from 'redux-observable'
-import { AppState, ReduxAction } from '../types'
-
-const logEpic: Epic<ReduxAction, AppState> = action$ => {
-    return action$.do(console.log).filter(() => false)
-}
-
-export default logEpic
--- src/store/rootEpic.ts
+++ src/store/rootEpic.ts
@@ -3,17 +3,12 @@ import { combineEpics, Epic } from 'redux-observable'
 
 import { AppState, ReduxAction } from './types'
 import languageEpic from './language/epic'
-import logEpic from './logger/epic'
 // Import epic here
 
 const epics = [
     languageEpic,
     // Insert epic here
 ]
-
-if (process.env.NODE_ENV === 'development') {
-    epics.push(logEpic)
-}
 
 const rootEpic: Epic<ReduxAction, AppState> = combineEpics(...epics)
 
