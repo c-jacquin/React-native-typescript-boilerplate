cd3c021e747c5646a6fefccea89f6774794934ea
Charles Jacquin <charles.jacquin@autistici.org>
2017-07-25 17:42:35 +0200

    test(language): add missing test in for api

--- src/store/language/api.ts
+++ src/store/language/api.ts
@@ -1,8 +1,10 @@
 import { Observable } from 'rxjs'
 import { Util } from 'expo'
 
+export const formatLocale = (locale: string) => locale.split('_')[1]
+
 export const getLanguage = () => {
     return Observable.fromPromise(Util.getCurrentLocaleAsync()).map(
-        locale => locale.split('_')[0]
+        formatLocale
     )
 }
