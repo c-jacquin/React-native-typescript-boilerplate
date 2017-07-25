f0d857c1c732bb38f5b43e5c54ffa88d3e127f9f
Charles Jacquin <charles.jacquin@autistici.org>
2017-07-25 17:42:35 +0200

    test(language): add missing test in for api

--- src/store/language/__tests__/api.test.ts
+++ src/store/language/__tests__/api.test.ts
@@ -2,6 +2,10 @@ import * as languageApi from '../api'
 import { Util } from 'expo'
 
 describe('languages api', () => {
+    it('should properly format the locale', () => {
+        expect(languageApi.formatLocale('EN_en')).toBe('en')
+    })
+
     it('should get the current locale', () => {
         const spy = jest.spyOn(Util, 'getCurrentLocaleAsync')
 
