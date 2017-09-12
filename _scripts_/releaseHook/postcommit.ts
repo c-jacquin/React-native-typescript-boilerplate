7b0f7e43fe02eba13e04389fc2c1da15a271bcd5
Charles Jacquin <charles.jacquin@autistici.org>
2017-09-12 12:31:06 +0200

    chore(buildAndroid): add a npm script build:android

--- _scripts_/releaseHook/postcommit.ts
+++ _scripts_/releaseHook/postcommit.ts
@@ -1,32 +1,7 @@
 (async () => {
     const exec = require('child-process-promise').exec
     const jsonFormat = require('json-format')
-    const { readFile, writeFile } = require('../docs/helpers/fs')
-
-    const cleanGitignore = (): void => {
-        const gitignorePath = `${process.cwd()}/.gitignore`
-        const gitignioreContent = readFile(gitignorePath)
-
-        writeFile(gitignorePath, gitignioreContent
-            .replace(new RegExp('docs/', 'g'), '#docs/')
-            .replace(new RegExp('build/', 'g'), '#build/')
-        )
-    }
-
-    const updateExpoVersion = (): void => {
-        const expoConfigPath = `${process.cwd()}/app.json`
-        const pkg = require(`${process.cwd()}/package.json`)
-        const expoConfigContent = JSON.parse(readFile(expoConfigPath))
-        expoConfigContent.expo.version = pkg.version
-
-        writeFile(
-            expoConfigPath,
-            jsonFormat(expoConfigContent, {
-                type: 'space',
-                size: 2,
-            })
-        )
-    }
+    const { cleanGitignore, updateExpoVersion } = require('./lib')
 
     try {
         cleanGitignore()
