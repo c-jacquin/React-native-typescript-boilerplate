ceff67a01ae3bd184eca27016bfcf171ac6f8220
Charles Jacquin <charles.jacquin@autistici.org>
2017-09-16 18:30:50 +0200

    chore(release): update changelog in postcommit hook

--- _scripts_/releaseHook/posttag.ts
+++ _scripts_/releaseHook/posttag.ts
@@ -1,16 +1,11 @@
 (async () => {
-    const exec = require('child-process-promise').exec
-    const { restoreGitignore } = require('./lib')
+    const { exec } = require('child-process-promise')
+    const gitBranch = require('git-branch')
 
     try {
-        restoreGitignore()
-        await exec('git push --follow-tags --no-verify origin master')
-        await exec('npm run clean')
-        await exec('npm run add')
-        await exec('git commit -m "chore(build): remove documentation and build" --no-verify')
-        await exec('git push --no-verify origin master')
+        await exec(`git push --follow-tags --no-verify origin ${gitBranch.sync()}`)
     } catch (err) {
         console.error(err)
-        process.exit(0)
+        process.exit(1)
     }
 })()
