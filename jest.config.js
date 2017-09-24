a0eb232b16f757c53cd01c4d945b49886226a943
Charles Jacquin <charles.jacquin@autistici.org>
2017-09-24 20:36:14 +0200

    test(config): externalize jest config (jest.config.js)

---
+++ jest.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    globals: {
+        'ts-jest': {
+            tsConfigFile: 'tsconfig-test.json'
+        }
+    },
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/**/*.test.{ts, tsx}',
+        '!src/**/*.d.ts',
+        '!src/**/types.ts',
+        '!**/node_modules/**'
+    ],
+    coverageDirectory: '.temp',
+    moduleDirectories: [
+        'node_modules',
+        'src'
+    ],
+    moduleFileExtensions: [
+        'js',
+        'ts',
+        'tsx'
+    ],
+    preset: 'jest-expo-ts',
+    setupFiles: [
+        './_scripts_/testHook/test-setup.ts'
+    ],
+    testPathIgnorePatterns: [
+        '<rootDir>/build/',
+        '<rootDir>/node_modules/'
+    ],
+    transform: {
+        '.(js|jsx)': '<rootDir>/node_modules/babel-jest',
+        '.(ts|tsx)': '<rootDir>/node_modules/ts-jest/preprocessor.js'
+    },
+    transformIgnorePatterns: [
+        'node_modules/(?!(jest-)?|glamorous-native)'
+    ],
+    testResultsProcessor: './node_modules/jest-html-reporter'
+}
