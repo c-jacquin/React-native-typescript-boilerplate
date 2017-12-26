1d641583f8a1b9a790a6dd73f7d7d202894ce4fe
Charles Jacquin <charles.jacquin@autistici.org>
2017-12-26 18:45:09 +0100

    style(language): remove useless import

--- src/store/language/selectors.ts
+++ src/store/language/selectors.ts
@@ -1,10 +1,8 @@
-import { createSelector, OutputSelector, Selector } from 'reselect'
+import { createSelector, Selector } from 'reselect'
 import { AppState } from '../types'
 import { LanguageState } from './types'
 
-export const selectLanguage: Selector<AppState, LanguageState> = (
-    state: AppState
-) => state.language
+export const selectLanguage = (state: AppState) => state.language
 
 export const selectLocale: Selector<AppState, string> = createSelector(
     [selectLanguage],
